6708408c777f97cebc0006ecef118e034e1a988e
arefehgd96-spec <arefehgd96@gmail.com>
2026-03-05 17:45:49 +0100

    Refactor AIChat component to update content

--- src/pages/AIChats.jsx
+++ src/pages/AIChats.jsx
@@ -1,10 +1,3 @@
-export default function AIChat() {
-  return (
-    <div style={{ padding: 16 }}>
-      <h1>AI Chat</h1>
-      <p>Coming soon</p>
-    </div>
-  );
-}
+
 export default function AIChat() {
   return <div>AIChat placeholder</div>div>;
