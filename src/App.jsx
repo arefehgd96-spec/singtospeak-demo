bdf155435fe278af8071d294af06d0032a77d7aa
arefehgd96-spec <arefehgd96@gmail.com>
2026-03-05 15:56:04 +0100

    Remove AIChat import and entry from PAGES
    Removed AIChat import from App.jsx and PAGES object.

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ import NavigationTracker from '@/lib/NavigationTracker'
  *
  * The mainPage value must match a key in the PAGES object exactly.
  */
-import AIChat from "./pages/AIChat.jsx";
+
 import Explore from './pages/Explore';
 import Favorites from './pages/Favorites';
 import Games from './pages/Games';
@@ -69,7 +69,7 @@ import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
-    "AIChat": AIChat,
+    
     "Explore": Explore,
     "Favorites": Favorites,
     "Games": Games,
