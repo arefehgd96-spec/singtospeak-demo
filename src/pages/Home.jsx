868e825821a5cf5c0c1ba93a50fdf318836613cc
arefehgd96-spec <arefehgd96@gmail.com>
2026-03-05 18:21:58 +0100

    Create Home component for demo home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 16 }}>
+      <h1>Home</h1>
+      <p>Demo Home page</p>
+      <a href="/player">Go to Player</a>
+    </div>
+  );
+}
