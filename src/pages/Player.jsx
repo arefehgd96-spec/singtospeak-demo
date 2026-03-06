aa7fa2004941078c76931a5b7e2180fb7e2d34d5
arefehgd96-spec <arefehgd96@gmail.com>
2026-03-06 16:37:57 +0100

    Use Link component for navigation in Player
    Replaced the back navigation link with React Router's Link component.

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -1,17 +1,2 @@
-import { useRef } from "react";
-
-export default function Player() {
-  const audioRef = useRef(null);
-  const demoUrl =
-    "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3";
-
-  return (
-    <div style={{ padding: 16 }}>
-      <h1>Player</h1>
-      <audio ref={audioRef} src={demoUrl} controls style={{ width: "100%" }} />
-      <div style={{ marginTop: 12 }}>
-        <a href="/">Back to Home</a>
-      </div>
-    </div>
-  );
-}
+import { Link } from "react-router-dom";
+<Link to="/">Back to Home</Link>
