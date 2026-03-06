f99979b78b8e84335ea5a83a813096ab38ac4975
arefehgd96-spec <arefehgd96@gmail.com>
2026-03-06 21:28:21 +0100

    Create songs.js

---
+++ src/data/songs.js
@@ -0,0 +1,16 @@
+export const songs = [
+  {
+    id: "1",
+    title: "Demo Song 1",
+    artist: "Singer A",
+    url: "https://filesamples.com/samples/audio/mp3/sample3.mp3",
+    cover: "",
+  },
+  {
+    id: "2",
+    title: "Demo Song 2",
+    artist: "Singer B",
+    url: "https://filesamples.com/samples/audio/mp3/sample2.mp3",
+    cover: "",
+  },
+];
