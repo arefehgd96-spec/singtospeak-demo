ce446b12b7fb85cc7c2d06cf41e535f83d931e55
arefehgd96-spec <arefehgd96@gmail.com>
2026-03-06 17:38:45 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,9 @@
- <a href="/player">Go to Player</a>
+export default function Home() {
+  return (
+    <div style={{ padding: 16 }}>
+      <h1>HOME OK</h1>
+      <p>If you see this, Home is loading.</p>
+      <a href="/player">Go to Player</a>
     </div>
   );
 }
