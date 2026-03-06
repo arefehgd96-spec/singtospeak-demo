81a9f4c992f3509759d55948708ae57dbe5c4f0c
arefehgd96-spec <arefehgd96@gmail.com>
2026-03-06 18:52:21 +0100

    Refactor Home component and update links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,9 @@
-import { Link } from "react-router-dom";
-
 export default function Home() {
   return (
-    <div style={{ padding: 20, fontFamily: "system-ui" }}>
-      <h1 style={{ margin: 0 }}>Home</h1>
-      <p style={{ marginTop: 6, color: "#555" }}>Demo Home page</p>
-
-      <Link
-        to="/player"
-        style={{
-          display: "inline-block",
-          padding: "10px 14px",
-          marginTop: 10,
-          background: "#111",
-          color: "#fff",
-          borderRadius: 10,
-          textDecoration: "none",
-        }}
-      >
-        Go to Player
-      </Link>
+    <div style={{ padding: 20 }}>
+      <h1>HOME OK</h1>
+      <p>If you see this, Home is rendering.</p>
+      <a href="/player">Go to Player</a>
     </div>
   );
 }
