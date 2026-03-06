3538fe74577635b9678f99dd47966dc813c219e9
arefehgd96-spec <arefehgd96@gmail.com>
2026-03-06 16:47:14 +0100

    Replace Link component with anchor tag

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,4 @@
-import { Link } from "react-router-dom";
-
-export default function Home() {
-  return (
-    <div style={{ padding: 16 }}>
-      <h1>Home</h1>
-      <p>Demo Home page</p>
-      <Link to="/player">Go to Player</Link>
+ <a href="/player">Go to Player</a>
     </div>
   );
 }
