e475603fae964b65d6afce13c44d30217b1934ba
arefehgd96-spec <arefehgd96@gmail.com>
2026-03-08 14:59:48 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+export default function Home() {
+  return (
+    <div style={{padding: 40, fontSize: 30, color: "red"}}>
+      HOME VERSION 999
+    </div>
+  );
+}
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Player from "./pages/Player";
