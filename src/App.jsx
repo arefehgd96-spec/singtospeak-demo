b8f664ad183e79f155656c3c51d5bbbbeda0eff0
arefehgd96-spec <arefehgd96@gmail.com>
2026-03-08 15:02:20 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,3 @@ export default function Home() {
     </div>
   );
 }
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Player from "./pages/Player";
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/player" element={<Player />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
